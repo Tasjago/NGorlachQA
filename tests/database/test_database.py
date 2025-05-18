394544619f4c364f83af437a08dceda8d253123c
Natalia Gorlach <tasjago@gmail.com>
2025-05-18 02:06:58 +0100

    Project task 5. Individual part: additional DB tests

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -68,4 +68,55 @@ def test_detailed_orders():
     assert orders [0][1] == 'Sergii'
     assert orders [0][2] == 'солодка вода'
     assert orders [0][3] == 'з цукром'
-   
+
+
+# New tests for the individual part of Project Task 5
+
+@pytest.mark.database
+def test_insert_invalid_data_type():
+    db = Database()
+    with pytest.raises(Exception):
+        db.inser_product(5, 'молоко', 'біла рідина', 'not_a_number')  # qnt should be an integer
+
+
+@pytest.mark.database
+def test_insert_negative_quantity():
+    db = Database()
+    db.inser_product(6, 'йогурт', 'ягідний', -5)
+    qnt = db.select_product_qnt_by_id(6)
+    assert qnt[0][0] == -5  # SQLite allows it, test to check unexpected values
+
+
+@pytest.mark.database
+def test_update_nonexistent_product():
+    db = Database()
+    db.update_product_qnt_by_id(999, 10)  # Product ID 999 does not exist (if not used)
+    qnt = db.select_product_qnt_by_id(999)
+    assert qnt == []
+
+
+@pytest.mark.database
+def test_multiple_inserts_and_total():
+    db = Database()
+    db.inser_product(10, 'печиво1', 'тест1', 5)
+    db.inser_product(11, 'печиво2', 'тест2', 10)
+    db.inser_product(12, 'печиво3', 'тест3', 15)
+    total = db.select_product_qnt_by_id(10)[0][0] + \
+            db.select_product_qnt_by_id(11)[0][0] + \
+            db.select_product_qnt_by_id(12)[0][0]
+    assert total == 30
+
+
+@pytest.mark.database
+def test_join_query_returns_valid_structure():
+    db = Database()
+    orders = db.get_detailed_orders()
+    assert isinstance(orders, list)
+    assert len(orders[0]) == 5  # id, name, product_name, description, date
+
+
+@pytest.mark.database
+def test_null_product_id():
+    db = Database()
+    with pytest.raises(Exception):
+        db.select_product_qnt_by_id(None)       
